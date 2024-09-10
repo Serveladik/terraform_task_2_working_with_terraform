e16cef953f24d37af147c927b279273ed4c8c723
Serveladus <serveladik@gmail.com>
2024-09-10 14:16:18 +0300

    Solution

--- main.tf
+++ main.tf
@@ -1,25 +1,26 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "3.105.0"
     }
   }
 }
 
-provider "azurerm" {
-  features {}
+resource "azurerm_resource_group" "mate-terraform-rg" {
+  name     = "mate-terraform-resources"
+  location = var.location
 }
 
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
-  location = "West Europe"
-}
-
-resource "azurerm_storage_account" "example" {
-  name                     = "examplestorageacc"
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
+resource "azurerm_storage_account" "mate-terraform-sa" {
+  name                     = "mateterraformstorage"
+  resource_group_name      = azurerm_resource_group.mate-terraform-rg.name
+  location                 = azurerm_resource_group.mate-terraform-rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
+
+  tags = {
+    environment = "Staging"
+    owner = local.owner
+  }
 }
